0b6085e9045999ce473795071eb2ad5d4b2dda5a
(sshyukk) <(sanghyuk0328@gmail.com)>
2021-06-16 22:39:34 +0900

    stage6 update

---
+++ step-by-step/stage6/BOJ_15596.py
@@ -0,0 +1,10 @@
+# def solve(a):
+#     total = 0
+#     for i in a:
+#         total += i
+#     return total
+
+
+def solve(a):
+    return sum(a)
+        
---
+++ step-by-step/stage6/BOJ_4673.py
@@ -0,0 +1,8 @@
+N = list(input())
+
+for i in range(len(N)):
+    cons = int(N[i]*10 + N)
+
+
+
+
