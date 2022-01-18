8b3337062c15c912b9a58f0370e7e9a51a2f29d1
sshyukk <sanghyuk0328@gmail.com>
2022-01-18 11:45:31 +0900

    큰 수 출력하기(수정)

--- JavaScript/section_2/1. 큰 수 출력하기.js
+++ JavaScript/section_2/1. 큰 수 출력하기.js
@@ -1,11 +1,10 @@
-function solution(number, array) {
-  let answer = [];
-  answer = array.filter((value, index) => {
-    return value >= number;
-  });
+function solution(array) {
+  let answer = [array[0]];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > array[i - 1]) answer.push(array[i]);
+  }
   return answer;
 }
 
-let number = 6;
 let array = [7, 3, 9, 5, 6, 12];
-console.log(solution(number, array));
+console.log(solution(array));
