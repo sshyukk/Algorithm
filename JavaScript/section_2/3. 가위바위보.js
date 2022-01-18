b39c2c590ebbd683062c689541edc38e0c22d034
sshyukk <sanghyuk0328@gmail.com>
2022-01-18 11:46:07 +0900

    가위바위보(수정)

--- JavaScript/section_2/3. 가위바위보.js
+++ JavaScript/section_2/3. 가위바위보.js
@@ -1,18 +1,19 @@
-function solution(number, array) {
+function solution(array) {
   let answer = [];
-  for (let i = 0; i < number; i++) {
-    if (array[0][i] > array[1][i]) {
-    } else if (array[0][i] < array[1][i]) {
-    } else {
-      console.log("D");
-    }
+  let A = array[0];
+  let B = array[1];
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] === B[i]) answer.push("D");
+    else if (A[i] === 1 && B[i] === 3) answer.push("A");
+    else if (A[i] === 2 && B[i] === 1) answer.push("A");
+    else if (A[i] === 3 && B[i] === 2) answer.push("A");
+    else answer.push("B");
   }
-
   return answer;
 }
-let number = 5;
+
 let array = [
   [2, 3, 3, 1, 3],
   [1, 1, 2, 2, 3],
 ];
-console.log(solution(number, array));
+console.log(solution(array));
