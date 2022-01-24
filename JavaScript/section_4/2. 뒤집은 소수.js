9239b1d3d3b74e7cbff08f35d061794fe507821d
sshyukk <sanghyuk0328@gmail.com>
2022-01-24 16:40:25 +0900

    뒤집은 소수

--- JavaScript/section_4/2. 뒤집은 소수.js
+++ JavaScript/section_4/2. 뒤집은 소수.js
@@ -1,7 +1,20 @@
-function solution(string) {
-  let answer;
+// 소수 판별 함수.
+function isPrime(number) {
+  if (number === 2) return true;
+  for (let i = 2; i <= Math.floor(Math.sqrt(number)); i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
 
-  return answer;
+function solution(string) {
+  array = string.split(" ");
+  for (let x of array) {
+    let temp = Number(x.split("").reverse().join(""));
+    // console.log(temp);
+    if (temp === 1) break;
+    if (isPrime(temp)) console.log(temp);
+  }
 }
 
 let string = "32 55 62 20 250 370 200 30 100";
